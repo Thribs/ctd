5352537a56caf1399254c581b8dd5ed488a59652
Thribs <thribsilva@gmail.com>
2021-10-26 20:39:19 -0300

    aula 05

---
+++ programacao-imperativa-1/aula-04/aula-04.js
@@ -0,0 +1,18 @@
+console.log("Hello, world! 🍪");
+
+var x = 8;
+var y = 5;
+const z = 25;
+let h = 96;
+h = 78;
+x = 5;
+
+let nome = "Thiago";
+let plano = false;
+
+let soma = h + x;
+let sub = h - x;
+let mult = h * x;
+let div = h / x;
+
+console.log( nome, plano, soma, sub, mult, div );
---
+++ programacao-imperativa-1/aula-04/index.js
@@ -0,0 +1,9 @@
+const fullName = "Ana Paula";
+const age = 26;
+const mass = 55.0;
+const height = 1.62;
+const insurance = "prata"
+
+const bmi = mass / (height * height);
+
+console.log(`${fullName} tem ${age} anos de imade e seu IMC é ${bmi}`);
