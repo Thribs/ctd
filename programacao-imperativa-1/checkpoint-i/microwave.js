6a33a4db177cb7fb7538068d352ef433c93abd0e
Thribs <thribsilva@gmail.com>
2021-11-09 21:50:59 -0300

    checkpoint-i de Programação Imperativa

---
+++ programacao-imperativa-1/checkpoint-i/microwave.js
@@ -0,0 +1,59 @@
+const popcorn_time = 10;
+const pasta_time = 8;
+const meat_time = 15;
+const beans_time = 12;
+const brigadeiro_time = 8;
+
+function item_time(item) {
+  switch (item) {
+    case "pipoca":
+      return popcorn_time;
+    case "macarrão":
+      return pasta_time;
+    case "carne":
+      return meat_time;
+    case "feijão":
+      return beans_time;
+    case "brigadeiro":
+      return brigadeiro_time;
+    default:
+      return 0;
+  }
+}
+
+function cook(item, time) {
+  const correct_time = item_time(item);
+
+  if (correct_time == 0) {
+    console.log(`Prato ${item} não encontrado`);
+    return;
+  }
+
+  if (time < correct_time) {
+    console.log("Tempo insuficiente");
+    return;
+  }
+  if (time >= correct_time * 3) {
+    console.log("Kaboom!");
+    return;
+  }
+  if (time >= correct_time * 2) {
+    console.log(`O(a) ${item} queimou`);
+    return;
+  }
+  console.log("Prato pronto. Bom apetite!");
+}
+
+cook('batata', 50);
+cook('pipoca', 8);
+cook('macarrão', 18);
+cook('carne', 50);
+cook('feijão', -10);
+cook('brigadeiro', 9);
+
+// function cook(item, time) {
+//     const item_time = item_time(item)
+//     switch(time) {
+
+//     }
+// }
