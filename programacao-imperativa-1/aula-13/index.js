cab85a5d50ba523c34b868da334026479293e369
Thribs <thribsilva@gmail.com>
2021-11-22 22:46:10 -0300

    aula 13

---
+++ programacao-imperativa-1/aula-13/index.js
@@ -0,0 +1,12 @@
+let carro = {};
+carro.modelo = "Fusion"
+carro.cor = "branco"
+carro.andar = () => `${carro.modelo} ${carro.cor} rodando`;
+
+console.log(carro.andar());
+
+const object = {
+    "type test": "object"
+}
+
+console.log(object["type test"]);
