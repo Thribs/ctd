f9d62b3558198d847b7a1c9dd9caaa86f790deea
Thribs <thribsilva@gmail.com>
2021-11-24 22:14:51 -0300

    aula 15

---
+++ programacao-imperativa-1/aula-15/mesa/Account.js
@@ -0,0 +1,8 @@
+function Account(accNumber, accType, balance, owner) {
+    this.accNumber = accNumber;
+    this.accType = accType;
+    this.balance = balance;
+    this.owner = owner;
+}
+
+module.exports = Account;
