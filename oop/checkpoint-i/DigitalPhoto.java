9a8261802facb8a61ffbcd4a7b59ac7e605030cf
ThribsClube <thiago@clubedochaveiro.com.br>
2022-06-08 22:50:22 -0300

    Merging 'WIP: Do Not Merge This Branch' Into Master

---
+++ oop/checkpoint-i/DigitalPhoto.java
@@ -0,0 +1,7 @@
+import java.time.LocalDate;
+
+class DigitalPhoto extends Photo {
+    public DigitalPhoto(String title, String series, String captureLocation, LocalDate captureDate) {
+        super(title, series, captureLocation, captureDate);
+    }
+}
---
+++ oop/checkpoint-i/FilmPhoto.java
@@ -0,0 +1,7 @@
+import java.time.LocalDate;
+
+class FilmPhoto extends Photo {
+    public FilmPhoto(String title, String series, String captureLocation, LocalDate captureDate) {
+        super(title, series, captureLocation, captureDate);
+    }
+}
